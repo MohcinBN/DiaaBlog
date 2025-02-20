33f6eebf7b59cf6a578c0891029beb9a5dd57555
@bounouaramouad10 <bounouaramouad10@gmail.com>
2025-02-20 21:38:15 +0100

    creation of factories and the seeders

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['title','content','featured_image','user_id '];
 
     public function comment(){
