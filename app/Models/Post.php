13c1965cdc39e771a9c64fd95e2a8780f61b9dc0
MohcinBN <bounouara.mohcin@gmail.com>
2025-08-02 12:22:30 +0200

    Adding pivote table for post-category and reletionship for post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,4 +31,8 @@ class Post extends Model
     public function comments() {
         return $this->hasMany(Comment::class);
     }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
 }
